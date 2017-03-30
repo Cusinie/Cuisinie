ed9d6d4619ed8d940f35ad53fbf4be6c728cfc29
Paul Sanabria <pandres.sanabria@gmail.com>
2017-03-30 19:30:22 -0400

    goal day 1 complete Rendering search results to index

--- cuisinie/config/routes.rb
+++ cuisinie/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'restaurants#index'
   resources :restaurants
+  resources :cuisines
 
+  resources :apis
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
